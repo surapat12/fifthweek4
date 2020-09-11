42b8bdd8611caa9dad764f644518228728c71afd
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2020-09-12 01:17:53 +0700

    Update fifthweek4.cpp

--- fifthweek4/fifthweek4.cpp
+++ fifthweek4/fifthweek4.cpp
@@ -4,11 +4,21 @@ int main()
 {
 	int num[7], x;
 	int i, j;
-	printf("Enter seven inteeger numbers : \n");
-	for (i = 0; i < 7; i++)
-	{
-		scanf_s("%d ", & num[i]);
-	}
+	printf("number #1 : ");
+	scanf_s("%d", &num[0]);
+	printf("number #2 : ");
+	scanf_s("%d", &num[1]);
+	printf("number #3 : ");
+	scanf_s("%d", &num[2]);
+	printf("number #4 : ");
+	scanf_s("%d", &num[3]);
+	printf("number #5 : ");
+	scanf_s("%d", &num[4]);
+	printf("number #6 : ");
+	scanf_s("%d", &num[5]);
+	printf("number #7 : ");
+	scanf_s("%d", &num[6]);
+	
 	for (i = 0; i < 6; i++)
 	{
 		for (j = 0; j < 6; j++)
